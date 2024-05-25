5f01d1af04c3cb43f6d55e54ffb33414881a0864
Sarthak Parashar <41594440+sarthakparashar@users.noreply.github.com>
2024-05-26 00:50:45 +0530

    remove duplicate line in setup jobs

--- setup_jobs.sh
+++ setup_jobs.sh
@@ -1,2 +1,2 @@
 toolforge jobs run stats-update --command www/python/src/update_stats.sh --image python3.11 --schedule "@daily" -o www/python/src/jobs.log
-toolforge jobs run stats-update --command www/python/src/update_stats.sh --image python3.11 --schedule "@daily" -o www/python/src/jobs.log
+toolforge jobs run active-users-update --command www/python/src/update_active_users_job.sh --image python3.11 --schedule "@monthly" -o www/python/src/jobs.log
